611587b424a76f107f2908157ff95fd861d9c0ee
kibantu45 <osinyamaxwel28@gmail.com>
2025-06-02 00:30:44 +0300

    Update Chemist.tsx

--- src/pages/Chemist.tsx
+++ src/pages/Chemist.tsx
@@ -87,7 +87,7 @@ const Chemist = () => {
       category: "vitamins", 
       prescription: false, 
       stock: 75,
-      image: "https://www.nicepng.com/maxp/u2e6a9i1o0i1o0r5/?auto=format&fit=crop&w=400&q=80"
+      image: "https://www.goodlife.co.ke/product/trust-condoms-24s/#product-gallery?auto=format&fit=crop&w=400&q=80"
     },
   ];
 
